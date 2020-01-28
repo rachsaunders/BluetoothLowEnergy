5afb29498883ec361d7a2e010eea739894f1954f
Rachel Saunders <rachelsaundersBA@gmail.com>
2020-01-28 20:58:47 +0000

    As it stands it can currently only scan for devices.

--- BLE/ViewController.swift
+++ BLE/ViewController.swift
@@ -5,14 +5,38 @@
 //  Created by Rachel Saunders on 28/01/2020.
 //  Copyright © 2020 Rachel Saunders. All rights reserved.
 //
+// Peripherals are devices that are scanned for, central scans for devices.
+// Peripherals are identified by UUID (universally unique ID)
 
 import UIKit
+import CoreBluetooth
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CBCentralManagerDelegate {
+    
+    // centralManagerDidUpdateState tells it its updated
+    // CBManagerState is the state of Core Bluetooth Manager
+    
+    func centralManagerDidUpdateState(_ central: CBCentralManager) {
+        if central.state == CBManagerState.poweredOn {
+            central.scanForPeripherals(withServices: nil, options: nil)
+            print ("Scanning...")
+        }
+    }
+    
+    // centralManager tells it its discovered a peripheral while scanning
+    
+    func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String : Any], rssi RSSI: NSNumber) {
+        print (peripheral.name ?? "no name")
+    }
+    
+    
+    //CBCentralManager scans and manages discovered/connected remote devices
+    var centralManager : CBCentralManager!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+        centralManager = CBCentralManager.init(delegate: self, queue: nil)
     }
 
 
